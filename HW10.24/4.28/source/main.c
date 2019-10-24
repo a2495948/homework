88bd06d6b62d0d6bd4ac2a06d03b3ec07f779b6f
a2495948 <a2495948@gmail.com>
2019-10-24 21:58:52 +0800

    f

--- HW10.24/4.28/source/main.c
+++ HW10.24/4.28/source/main.c
@@ -3,10 +3,10 @@
 
 int main(void)
 {
-	printf("Pleace enter 1 => 2 => 3 ,if worker have no payment: Enter '0' after enter paycode\n");
-	printf(" 1:(hourly worker)   2:(commission worker)   3:(piece worker) \n");
+	printf("Pleace enter 1 => 2 => 3 =>4 ,if worker have no payment: Enter '0' after enter paycode\n");
+	printf("1:(manager)  2:(hourly worker)   3:(commission worker)   4:(piece worker) \n");
 	int i,a=0;
-	float manager=0, hourlyworker=0, commissionworker=0, pieceworker=0;
+	float manager=0, hourlyworker=0, commissionworker=0, pieceworker=0 ,total=0;
 	while (1)
 	{
 		printf("Enter paycode(-1 to end):");
@@ -17,12 +17,19 @@ int main(void)
 		}
 		else if (i == 1)
 		{
+			printf("(manager) :your payment is: ");
+			scanf_s("%f", &manager);
+			printf("\n");
+			a++;
+		}
+		else if (i == 2)
+		{
 			printf("(hourly worker) :your payment is: ");
 			scanf_s("%f", &hourlyworker);
 			printf("\n");
 			a++;
 		}
-		else if (i == 2)
+		else if (i == 3)
 		{
 			commissionworker = 250 + 250 * 0.057;
 			printf("(commission worker) :your payment is:%f", commissionworker);
@@ -30,17 +37,17 @@ int main(void)
 			printf("\n");
 			a++;
 		}
-		else if (i == 3)
+		else if (i == 4)
 		{
 			printf("(pieceworker) :your payment is: ");
 			scanf_s("%f", &pieceworker);
 			printf("\n");
 			a++;
 		}
-		if (a == 3)
+		if (a == 4)
 		{
-			manager = (hourlyworker + commissionworker + pieceworker);
-			printf("Manager need to pay :%f\n", manager);
+			total = (manager + hourlyworker + commissionworker + pieceworker);
+			printf("Total payment is :$%f\n", total);
 		}
 	}
 			system("pause");
